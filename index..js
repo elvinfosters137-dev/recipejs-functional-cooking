491a18e3b8d4942a179fdb1aca7ac9df0ad01b80
ElvinFoster777 <@ElvinFoster777>
2026-01-19 00:12:41 +0530

    Moderate

--- index..js
+++ index..js
@@ -1,5 +1,5 @@
 const recipes = [
-  { name: "Pasta", difficulty: "Easy", time: 20 },
+  { name: "Pasta", difficulty: "Moderate", time: 20 },
   { name: "Biryani", difficulty: "Hard", time: 60 },
   { name: "Salad", difficulty: "Easy", time: 10 },
   { name: "Burger", difficulty: "Medium", time: 30 }
